b43a6f3ffbd7ea2d4aa6ceec316066c62c5ebc95
Dinuk weerasinghe <dinux98@gmail.com>
2024-03-18 10:38:10 +0530

    activity_Registration.XML - Designed

---
+++ app/src/main/java/com/example/shopie/RegistrationActivity.java
@@ -0,0 +1,25 @@
+package com.example.shopie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+
+
+    public void signup(View view){
+
+    }
+
+    public void signin(View view){
+
+    }
+
+}
