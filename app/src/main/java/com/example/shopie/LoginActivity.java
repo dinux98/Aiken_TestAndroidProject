cbd242e8a15370911d5dc5498323aa6b1a51696e
Dinuk weerasinghe <dinux98@gmail.com>
2024-03-18 16:14:46 +0530

    Add registration and login functionality
    Implemented registration and login functionalities using firebase.

--- app/src/main/java/com/example/shopie/LoginActivity.java
+++ app/src/main/java/com/example/shopie/LoginActivity.java
@@ -1,21 +1,78 @@
 package com.example.shopie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    Button signup;
+    EditText email , password;
+    TextView signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.et_email);
+        password = findViewById(R.id.et_password);
+
+        signup = findViewById(R.id.btn_signup);
+        signin = findViewById(R.id.tv_signin);
+
+        auth = FirebaseAuth.getInstance();
+
     }
 
     public void signin(View view){
-        startActivity(new Intent(LoginActivity.this,LoginActivity.class));
+
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+
+        if (TextUtils.isEmpty(userEmail)) {
+            Toast.makeText(this, "Email is Required!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (userPassword.length()< 6) {
+            Toast.makeText(this, "Password is too short ,Use at least 6 characters!", Toast.LENGTH_SHORT).show();
+
+        }
+
+        auth.signInWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+                        }else {
+                            Toast.makeText(LoginActivity.this, "Sign Up before Login", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
+
     }
 
     public void signup(View view){
--- app/src/main/java/com/example/shopie/RegistrationActivity.java
+++ app/src/main/java/com/example/shopie/RegistrationActivity.java
@@ -1,42 +1,90 @@
 package com.example.shopie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
-    
+
     Button signup;
     EditText name , email , password;
     TextView signin;
+
+    private static final String TAG = "Registration";
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        
+
+        name = findViewById(R.id.et_name);
+        email = findViewById(R.id.et_email);
+        password = findViewById(R.id.et_password);
+
         signup = findViewById(R.id.btn_signup);
         signin = findViewById(R.id.tv_signin);
-        
-//        signup.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(RegistrationActivity.this,MainActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+
+        auth = FirebaseAuth.getInstance();
+
 
 
     }
 
 
     public void signup(View view){
-        startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+
+        String userName = name.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+
+
+            if (TextUtils.isEmpty(userName)) {
+                Toast.makeText(this, "User Name is Required!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(userEmail)) {
+                Toast.makeText(this, "Email is Required!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (userPassword.length() < 6) {
+                Toast.makeText(this, "Password is too short ,Use at least 6 characters!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
+        auth.createUserWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            Toast.makeText(RegistrationActivity.this, "Successfully Registered !", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+                        }else {
+                            Toast.makeText(RegistrationActivity.this, "Registration Failed !" +task.getException(), Toast.LENGTH_SHORT).show();
+                            Log.i(TAG, "Signup:");
+                        }
+                    }
+                });
+
+
     }
     public void signin(View view){
         startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
